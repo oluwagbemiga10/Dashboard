6b6293648871b8407ca100faefa790921a44e17b
oluwagbemiga10 <oyelamigbenga11@gmail.com>
2024-05-27 01:20:39 +0100

    setting up the folders

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from '../components/dashboard/sidebar/sidebar'
+import Navbar from '../components/dashboard/navbar/navbar'
+import styles from '../components/dashboard/dashboard.module.css'
+
+const Layout = ({children}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
