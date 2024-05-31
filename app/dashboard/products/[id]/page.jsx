81d687bd027e093072d1f66e53169ee29f18337c
oluwagbemiga10 <oyelamigbenga11@gmail.com>
2024-05-31 04:06:14 +0100

    product page files

---
+++ app/dashboard/products/[id]/page.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from '../../../components/dashboard/products/singleproduct/singleproduct.module.css'
+import Image from 'next/image'
+
+const Singleproduct = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+            <Image src="/noavatar.png" alt='' fill />
+        </div>
+        Iphone
+      </div>
+      <div className={styles.formContainer}>
+        <form action='' className={styles.form}>
+        <label>Title</label>
+        <input type='text' name='title' placeholder='Iphone' />
+        <label>Price</label>
+        <input type='number' name='price' placeholder='$74.00' />
+        <label>Stock</label>
+        <input type='number' name='stock' placeholder='23' />
+        <label>Color</label>
+        <input type='text' name='color' placeholder='red' />
+        <label>Size</label>
+        <input type='text' name='size' placeholder='45' />
+        <label>Cat</label>
+        <select name="cat" id="cat">
+            <option value='kitchen'>Kitchen</option>
+            <option value='computer'>Computers</option>
+        </select>
+        <label>Description</label>
+        <textarea name='desc' id='desc' rows='10' placeholder='description'></textarea>
+        <button>Update</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Singleproduct
--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import styles from "../../components/dashboard/products/product.module.css"
+import Search from '../../components/dashboard/search/search'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '../../components/dashboard/pagination/pagination'
 
 const ProductPage = () => {
   return (
-    <div>
-      <p>Products</p>
+    <div className={styles.container}>
+    <div className={styles.top}>
+      <Search placeholder="Search for a user..." />
+      <Link href="/dashboard/products/add">
+        <button className={styles.addButton}>Add New</button>
+      </Link>
     </div>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+        <td>Title</td>
+        <td>Description</td>
+        <td>Price</td>
+        <td>Created At</td>
+        <td>Stock</td>
+        <td>Action</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className={styles.product}>
+              <Image src="/noproduct.jpg" alt='' width={40} height={40} className={styles.productImage} />
+              Iphone
+            </div>
+          </td>
+          <td>Desc</td>
+          <td>2000</td>
+          <td>13.04.2024</td>
+          <td>72</td>
+          <td>
+            <div className={styles.buttons}>
+              <Link href='/dashboard/products/test'>
+                <button className={`${styles.button} ${styles.view}`} >view</button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <Pagination />
+  </div>
   )
 }
 
