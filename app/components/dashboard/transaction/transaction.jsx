df09dd250350103f9d77d0ef235d3a798bd4cf93
oluwagbemiga10 <oyelamigbenga11@gmail.com>
2024-05-31 04:04:15 +0100

    transcaction page completed

---
+++ app/components/dashboard/transaction/transaction.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import styles from './transaction.module.css'
+import Image from 'next/image'
+
+const Transaction = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transaction</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+              <Image src='/noavatar.png' alt='' width={40} height={40} className={styles.userImage} />
+              Gbenga Oyelami
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+            <div className={styles.user}>
+              <Image src='/noavatar.png' alt='' width={40} height={40} className={styles.userImage} />
+              Gbenga Oyelami
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Pending</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+            <div className={styles.user}>
+              <Image src='/noavatar.png' alt='' width={40} height={40} className={styles.userImage} />
+              Gbenga Oyelami
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>Pending</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+          <tr>
+            <td>
+            <div className={styles.user}>
+              <Image src='/noavatar.png' alt='' width={40} height={40} className={styles.userImage} />
+              Gbenga Oyelami
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$3.200</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Transaction
